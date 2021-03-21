08d05d1d256f4f0f7f821e1f83526ef4728fe68b
BuildTools <unconfigured@null.spigotmc.org>
2021-03-20 22:47:30 -0700

    Practica #7

--- app/src/main/java/mx/edu/itson/popcornfactory/DetallePelicula.kt
+++ app/src/main/java/mx/edu/itson/popcornfactory/DetallePelicula.kt
@@ -1,5 +1,6 @@
 package mx.edu.itson.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detalle_pelicula.*
 import android.os.Bundle
@@ -11,11 +12,31 @@ class DetallePelicula : AppCompatActivity() {
         setContentView(R.layout.activity_detalle_pelicula)
 
         val bundle = intent.extras
+        var ns = 0;
+        var id = -1;
+        var title = "";
 
         if (bundle != null){
+            ns = bundle.getInt("numberSeats")
+            title = bundle.getString("titulo")!!
             iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("nombre"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seatLeft.setText("$ns seats available ")
+            id = bundle.getInt("pos")
+        }
+
+        if(ns == 0){
+            buyTicket.isEnabled = false
+        } else {
+            buyTicket.setOnClickListener{
+                val intent: Intent = Intent(this, SeatSelection::class.java)
+
+                intent.putExtra("id", id)
+                intent.putExtra("nombre", title)
+
+                this.startActivity(intent)
+            }
         }
     }
 }
